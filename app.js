473bce2db6e0d685aab4fe3aba46eedc5d829467
Oleg Pukhaev <olegpukhaev@gmail.com>
2021-08-04 14:32:52 +0600

    Connect to db

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//mongoose test
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+// Mongoose test
+
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 app.get('*', (req, res) => {
